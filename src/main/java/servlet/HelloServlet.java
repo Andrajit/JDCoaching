d94193fef054502e293c841a49e3360399fe8f68
Andrajit <alex.clement.giraud@gmail.com>
2018-03-28 08:47:27 +0200

    debut sprint 1

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
+import BD.bd;
+import METIER.utilisateur;
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -9,24 +12,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet Hello World.
- * @author Alexandre
- */
 @WebServlet(
-        name = "MyServlet",
+        name = "MyServlet", 
         urlPatterns = {"/hello"}
     )
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected final void doGet(final HttpServletRequest req,
-            final HttpServletResponse resp)
-            throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
-        out.flush();
-        out.close();
-    }
+     public void doGet(HttpServletRequest requete,
+                      HttpServletResponse reponse) throws ServletException, IOException
+    {
+        String code, nom,prenom,datenaissance,mail,tel,statuts,password,type;
+        RequestDispatcher rd;
+        code=requete.getParameter("codeu");
+        nom=requete.getParameter("nom");
+        prenom=requete.getParameter("prenom");
+        datenaissance=requete.getParameter("datenaissance");
+        mail=requete.getParameter("mail");
+        tel=requete.getParameter("tel");
+        statuts=requete.getParameter("statuts");
+        password=requete.getParameter("password");
+        type=requete.getParameter("type");
+        utilisateur m=new utilisateur(code,nom,prenom,datenaissance,mail,tel,statuts,password,type);
 
+       
+        bd b=new bd();
+        b.saisirUtilisateur(m);
+        
+        rd=requete.getRequestDispatcher("Confirmation.jsp");
+        rd.forward(requete, reponse);
+        
+    }
+    
 }
